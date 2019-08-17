a2da3b4de881a1859cab420fc15ca6e49e0e757f
hthuyet <lethuyet.10.11.1990@gmail.com>
2019-08-17 09:55:04 +0700

    commit

---
+++ src/main/java/com/cael/omr/utils/Utils.java
@@ -0,0 +1,33 @@
+package com.cael.omr.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+@Slf4j
+public class Utils {
+
+    public static String getValueOfArgs(String input, String key, String defaultValue) {
+        if (StringUtils.isBlank(input)) {
+            return defaultValue;
+        }
+        if (StringUtils.isBlank(key)) {
+            return defaultValue;
+        }
+        String tmp = input.toLowerCase();
+        String tmpKey = String.format("--%s", key);
+        if (StringUtils.startsWith(tmp, tmpKey)) {
+            return input.substring(tmpKey.length() + 1).trim();
+        }
+        return defaultValue;
+    }
+
+    public static String getValueOfArgs(String key, String defaultValue, String... args) {
+        String tmpKey = String.format("--%s", key);
+        for (String s : args) {
+            if (StringUtils.startsWith(s, tmpKey))
+                return getValueOfArgs(s, key, defaultValue);
+        }
+        log.warn("Not found Args {} --> Return value: {}", key, defaultValue);
+        return defaultValue;
+    }
+}
